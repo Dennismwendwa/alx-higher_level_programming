0a4ed811e2841948566cd20b9a60491f6eae759a
Dennismwendwa <dennismusemb2@.com>
2023-06-06 19:18:15 +0300

    chars print

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,6 +2,6 @@
 def islower(c):
     lowercase_range = range(ord("a"), ord("z") + 1)
     if ord(c) in lowercase_range:
-         return True
+        return True
     else:
-        return False 
+        return False
