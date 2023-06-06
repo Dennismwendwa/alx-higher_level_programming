01c4199da9965ce2955615fecf0a171d1cec2ec3
Dennismwendwa <dennismusemb2@.com>
2023-06-06 17:34:05 +0300

    ahl

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ch in range(ord("a"), ord("z") + 1):
-    print(chr(ch), end="")
+alph = ''.join(chr(letter) for letter in range(ord('a'), ord('z') + 1))
+print("{}".format(alph), end="")
