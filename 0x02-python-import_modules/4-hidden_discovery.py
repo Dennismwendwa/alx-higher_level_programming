c8cc999312fdf2b82f9f14b1a1ea2fe7f6ee9ad4
Dennismwendwa <dennismusemb2@.com>
2023-06-08 11:24:46 +0300

    printing names from .pyc

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    import hidden_4
+
+obj = dir(hidden_4)
+nams = list(obj)
+
+for nam in nams:
+    if nam[0] == "-":
+        break
+        print("{:s}".format(nam))
