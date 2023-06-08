eb4e828365e3ce236f47f1b31770bebe71498cae
Dennismwendwa <dennismusemb2@.com>
2023-06-08 15:02:25 +0300

    sum

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,25 +5,25 @@ if __name__ == "__main__":
     from sys import argv, exit
 
 
-if len(argv) == 4:
-    operator = argv[2]
-
-    a = (argv[1])
-    b = (argv[3])
-    match operator:
-        case "+":
-            print("{} + {} = {}".format(a, b, add(int(a), int(b))))
-        case "-":
-            print("{} - {} = {}".format(a, b, sub(int(a), int(b))))
-        case "*":
-            print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
-        case "/":
-            print("{} / {} = {}".format(a, b, div(int(a), int(b))))
-        case _:
-            print("{}".format("Unknown operator. Available operators:\
- +, -, * and /"))
-            exit(1)
-
-else:
+if len(argv) != 4:
     print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
     exit(1)
+
+
+operator = argv[2]
+
+a = (argv[1])
+b = (argv[3])
+match operator:
+    case "+":
+        print("{} + {} = {}".format(a, b, add(int(a), int(b))))
+    case "-":
+        print("{} - {} = {}".format(a, b, sub(int(a), int(b))))
+    case "*":
+        print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
+    case "/":
+        print("{} / {} = {}".format(a, b, div(int(a), int(b))))
+    case _:
+        print("{}".format("Unknown operator. Available operators:\
+ +, -, * and /"))
+        exit(1)
