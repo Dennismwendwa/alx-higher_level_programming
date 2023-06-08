721d548ca4128db17b7f600cdf099d40f88634b1
Dennismwendwa <dennismusemb2@.com>
2023-06-08 10:23:14 +0300

    sys argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,11 @@ import sys
 
 if len(sys.argv) == 2:
     print("{:d} argument.".format((len(sys.argv) - 1)))
-    print("{:d}: {:s}".format((len(sys.argv) -1), sys.argv[-1]))
+    print("{:d}: {:s}".format((len(sys.argv) - 1), sys.argv[- 1]))
 elif len(sys.argv) == 1:
     print("{:d} arguments:".format(len(sys.argv) - 1))
 elif len(sys.argv) > 2:
-    print("{:d} arguments:".format(len(sys.argv) -1))
+    print("{:d} arguments:".format(len(sys.argv) - 1))
 
     f = 1
     while f != len(sys.argv):
