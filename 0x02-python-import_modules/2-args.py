d14c8f35d8ce8b57c4fd901c7cdab11a05d5eed7
Dennismwendwa <dennismusemb2@.com>
2023-06-08 10:20:21 +0300

    sys argvs

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+
+if len(sys.argv) == 2:
+    print("{:d} argument.".format((len(sys.argv) - 1)))
+    print("{:d}: {:s}".format((len(sys.argv) -1), sys.argv[-1]))
+elif len(sys.argv) == 1:
+    print("{:d} arguments:".format(len(sys.argv) - 1))
+elif len(sys.argv) > 2:
+    print("{:d} arguments:".format(len(sys.argv) -1))
+
+    f = 1
+    while f != len(sys.argv):
+        print("{:d}: {}".format(f, sys.argv[f]))
+        f += 1
