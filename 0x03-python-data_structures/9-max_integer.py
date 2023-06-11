4093f8cfefcd5fd84d7a2cbbb2ddad7116faf7c0
Dennismwendwa <dennismusemb2@.com>
2023-06-11 21:35:46 +0300

    list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/urs/bin/python3
+
+def max_integer(my_list=[]):
+
+    if len(my_list) == 0:
+        retun None
+
+    mx = my_list[0]
+    for i in my_list:
+        if i > mx:
+            mx = i
+    return mx
