78c42f5729344511eca5dcbf793536c335d7e3f4
Dennismwendwa <dennismusemb2@.com>
2023-06-11 18:12:13 +0300

    index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,6 +4,6 @@ def element_at(my_list, idx):
     for _ in my_list:
         length += 1
 
-    if idx > length or idx < 0:
+    if idx > length - 1 or idx < 0:
         return None
     return my_list[idx]
