824366722f1fbc9b5f037373a48b4d80cee46399
Dennismwendwa <dennismusemb2@.com>
2023-06-14 15:34:20 +0300

    cals

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -21,6 +21,5 @@ if __name__ == "__main__":
     elif operator == "/":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
-        print("{}".format("""Unknown operator. Available operators:+, -, *\
-        and /"""))
+        print("Unknown operator. Available operators:+, -, * and /"))
         exit(1)
