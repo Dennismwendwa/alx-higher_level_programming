ac19b9795f7ebc2465db97221178e917d071b2d9
Dennismwendwa <dennismusemb2@.com>
2023-06-14 14:49:33 +0300

    pycodesytle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return 0
 
-    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-	    'D': 500, 'M': 1000}
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     reslt = 0
     prev_value = 0
 
     for x in roman_string[::-1]:
-        val = roman_values.get(x, 0)
+        val = roman.get(x, 0)
 
         if val < prev_value:
             reslt -= val
