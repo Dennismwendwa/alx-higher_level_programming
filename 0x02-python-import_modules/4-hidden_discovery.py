9a5849a01baeba056e3fdbfca9c8b807a13c2777
Dennismwendwa <dennismusemb2@.com>
2023-06-14 15:12:24 +0300

    hiden2

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,8 +4,6 @@ if __name__ == "__main__":
     import sys
     import hidden_4
 
-objects = list(dir(hidden_4))
-
-for item in objects:
-    if item[0] is not "_":
+for item in dir(hidden_4):
+    if item[0] is not '_':
         print("{:s}".format(item))
