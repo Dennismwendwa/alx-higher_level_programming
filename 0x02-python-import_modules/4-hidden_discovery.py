ffc47035e4ad5f349843cf4dd7989f1a3a2ce8b0
Dennismwendwa <dennismusemb2@.com>
2023-06-14 15:14:37 +0300

    hidden3

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
     import hidden_4
 
-for item in dir(hidden_4):
-    if item[0] is not '_':
-        print("{:s}".format(item))
+    for item in dir(hidden_4):
+        if item[0] != '_':
+            print("{:s}".format(item))
