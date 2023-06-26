75eaf2fa4af0bde21dc5d9105860a61a157b7a06
Dennismwendwa <dennismusembi2@gmail.com>
2023-06-26 14:55:02 +0300

    pycode

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -8,7 +9,7 @@ def magic_calculation(a, b):
                 raise Exception("Too far")
             result += (a ** b) / i
 
-        except:
-            result = result + a + b
+        except Exception:
+            result = a + b
             break
     return result
