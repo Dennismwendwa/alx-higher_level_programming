ad5b8c48633692136a838a1c1c7952ad24bf139d
Dennismwendwa <dennismusembi2@gmail.com>
2023-06-26 14:34:46 +0300

    When catching exceptions, mention specific exceptions when     possible

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,9 +3,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
-        return False
-    except ValueError:
+    except (TypeError, ValueError):
         return False
     else:
         return True
