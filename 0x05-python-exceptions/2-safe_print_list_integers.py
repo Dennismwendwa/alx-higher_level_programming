ce7d44cdc9bc4a88fb878ee5bff55db3afbbe031
Dennismwendwa <dennismusembi2@gmail.com>
2023-06-26 14:39:16 +0300

    Never use the characters 'l', 'O', or 'I' as variable names

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,9 +2,9 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    for l in range(x):
+    for h in range(x):
         try:
-            print("{:d}".format(my_list[l]), end="")
+            print("{:d}".format(my_list[h]), end="")
             count += 1
         except (ValueError, TypeError):
             pass
