84bc3bdb1522aaccd7c71439411c08ffea39141e
Dennismwendwa <dennismusembi2@gmail.com>
2023-06-28 04:41:28 +0300

    python pycode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This is class MagicClass"""
+
+
+class MagicClass:
+    """Attributes of the class"""
+
+    def __init__(self, radius=0):
+        """Attributes
+            radius: radius
+        """
+        self.__radius = 0
+
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    """This method returns area"""
+    def area(self):
+        return self.__radius **2 * math.pi
+
+    """This method return circumference"""
+    def circumference(self):
+        return 2 * math.pi * self.__radius
