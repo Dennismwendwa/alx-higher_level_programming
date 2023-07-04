1fd60ed243ba30073330eaef57980b6e8b10db9a
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-04 04:06:12 +0300

    added class public attributes

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -3,70 +3,72 @@
 This is class rectangle with four methods and three magic methods
 """
 
+
 class Rectangle:
-	"""These are the attributes of this class"""
-	number_of_instances = 0
-	def __init__(self, width=0, height=0):
-		self.width = width
-		self.height = height
-		Rectangle.number_of_instances += 1
+    """These are the attributes of this class"""
+    number_of_instances = 0
 
-	"""This method return rectangle shape in #"""
-	def __str__(self):
-		if self.__width == 0 or self.__height == 0:
-			return ""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
 
-		shape = ""
-		for _ in range(self.__height):
-			shape += "#" * self.__width + "\n"
-		return shape[:-1]
+    """This method return rectangle shape in #"""
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
 
-	"""This method is string representation of rectangle"""
-	def __repr__(self):
-		return f"Rectangle({self.__width}, {self.__height})"
+        shape = ""
+        for _ in range(self.__height):
+            shape += "#" * self.__width + "\n"
+        return shape[:-1]
 
-	"""This method gets called everytime instance is deleted"""
-	def __del__(self):
-		print("Bye rectangle...")
-		Rectangle.number_of_instances -= 1
+    """This method is string representation of rectangle"""
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
 
-	"""This method returns width"""
-	@property
-	def width(self):
-		return self.__width
+    """This method gets called everytime instance is deleted"""
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
-	"""this method sets width"""
-	@width.setter
-	def width(self, value):
-		if not isinstance(value, int):
-			raise TypeError("width must be an integer")
-		elif value < 0:
-			raise ValueError("width must be >= 0")
+    """This method returns width"""
+    @property
+    def width(self):
+        return self.__width
 
-		self.__width = value
+    """this method sets width"""
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
 
-	"""This method returns height"""
-	@property
-	def height(self):
-		return self.__height
+        self.__width = value
 
-	"""This method sets height"""
-	@height.setter
-	def height(self, value):
-		if not isinstance(value, int):
-			raise TypeError("height must be an integer")
-		elif value < 0:
-			raise ValueError("height must be >= 0")
+    """This method returns height"""
+    @property
+    def height(self):
+        return self.__height
 
-		self.__height = value
+    """This method sets height"""
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
 
-	"""This method returns area"""
-	def area(self):
-		return self.__width * self.__height
+        self.__height = value
 
-	"""this method return perimeter"""
-	def perimeter(self):
-		if self.__height == 0 or self.__height == 0:
-			return 0
+    """This method returns area"""
+    def area(self):
+        return self.__width * self.__height
 
-		return 2 * (self.__width + self.__height)
+    """this method return perimeter"""
+    def perimeter(self):
+        if self.__height == 0 or self.__height == 0:
+            return 0
+
+        return 2 * (self.__width + self.__height)
