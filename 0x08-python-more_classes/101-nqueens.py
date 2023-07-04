ac648a287b4dd8ef7f19dd028f45222b6abf7121
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-04 07:23:23 +0300

    Queens N game

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,68 +1,51 @@
 #!/usr/bin/python3
 """
 This class solves the N-queen puzzle challenge
+function is_safe - In this function we are checking if its safe
+to place queen at a specific position. "row, col" on the board
 """
-
 
 from sys import argv
 
-def if_safe(board, row, col):
+
+def is_safe(board, row, col):
 
     for i in range(row):
-        if board[i][col] == 1:
+        if (board[i] == col or
+                board[i] - i == col - row or
+                board[i] + i == col + row):
             return False
-
-    i = row
-    j = col
-
-    while i >= 0 and j >= 0:
-        if board[i][j] == 1:
-            return False
-
-        i -= 1
-        j -= 1
-
-    i = row
-    j = col
-
-    while i >= 0 and j < len(board):
-        if board[i][j] == 1:
-            return False
-
-        i -= 1
-        j += 1
-
     return True
+
 
 def nqueen_game(n):
 
-    def  util(board, row):
+    def util(board, row):
         if row == n:
-            print(board)
+            print([[i, board[i]] for i in range(n)])
             return
 
         for col in range(n):
-            if if_safe(board, row, col):
-                board[row][col] = 1
+            if is_safe(board, row, col):
+                board[row] = col
                 util(board, row + 1)
-                board[row][col] = 0
 
-        board = [[0] * n for _ in range(n)]
-        util(board, 0)
+    if not str(n).isdigit():
+        print("N must be a number")
+        exit(1)
+
+    n = int(n)
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    board = [-1] * n
+    util(board, 0)
+
 
 if __name__ == "__main__":
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
 
-    
-    N = int(argv[1])
-    if N < 4:
-        print("N must be at least 4")
-        exit(1)
-
-    if N.isdigit() is false:
-        print("N must be a number")
-        exit(1)
-
-    nqueen_game(N)
+    nqueen_game(argv[1])
