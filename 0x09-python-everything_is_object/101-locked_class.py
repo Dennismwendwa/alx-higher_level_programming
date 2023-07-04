10d87d41399d5ee636b13ce8273c2e3c60a26da5
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-04 15:48:12 +0300

    lock classes with __slots__

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""
+This class is locked no attributes, only the required ones
+"""
 
 
 class LockedClass:
+    """attributes here for this class"""
     __slots__ = ["first_name"]
