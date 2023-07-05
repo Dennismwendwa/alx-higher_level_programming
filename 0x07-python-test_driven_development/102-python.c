f7cc252b5917b8790a5039262740fc9fce8ba1bb
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-05 03:27:24 +0300

    cpython strings

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,36 +1,36 @@
 #include "cpython.h"
 
 /**
- * print_string - function that prints cpython strings
- * @cp:- pointer to obj
+ * print_python_string - function that printd cpython strings
+ * @p:- pointer to the object
  * Return:- Always 0
  */
 
 void print_python_string(PyObject *p)
 {
-	PyObject *str;
-	PyObject *repr;
+	PyUnicodeObject *unicode = (PyUnicodeObject *)p;
 
-	(void)repr;
 	printf("[.] string object info\n");
-
-	if (strcmp(p->ob_type->tp_name, "str"))
+	if (!PyUnicode_Check(p))
 	{
-		printf(" [ERROR] Invalid String Object\n");
+		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 
-	if (PyUnicode_IS_COMPACT_ASCII(p))
+	if (PyUnicode_IS_COMPACT_ASCII(unicode))
 	{
-		printf(" type: compact ascii\n");
+		printf("  type: compact ascii\n");
+		printf("  length: %ld\n", PyUnicode_GET_LENGTH(unicode));
+		printf("  value: %s\n", PyUnicode_AsUTF8(unicode));
+	}
+	else if (PyUnicode_IS_COMPACT(unicode))
+	{
+		printf("  type: compact unicode object\n");
+		printf("  length: %ld\n", PyUnicode_GET_LENGTH(unicode));
+		printf("  value: %ls\n", PyUnicode_AsWideCharString(unicode, NULL));
 	}
 	else
 	{
-		printf(" type: compact unicode object\n");
+		printf("  [ERROR] Invalid String Object\n");
 	}
-
-	repr = PyObject_Repr(p);
-	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
-	printf(" length: %ld\n", PyUnicode_GET_SIZE(p));
-	printf(" value: %s\n", PyBytes_AsString(str));
 }
