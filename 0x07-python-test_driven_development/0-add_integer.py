9966122a247686ea757dcee2f207c893b7f71e1c
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-06 06:13:38 +0300

    pystyle

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,19 +5,20 @@ The numbers must be ints or floats
 The result is always int
 """
 
+
 def add_integer(a, b=98):
-	"""
-	this function adds var a and var b
-	var a or be must be floats or ints
-	"""
+    """
+    this function adds var a and var b
+    var a or be must be floats or ints
+    """
 
-	if not isinstance(a, (int, float)):
-		raise TypeError("a must be an integer")
-	if not isinstance(b, (int, float)):
-		raise TypeError("b must be an integer")
-	if type(a) == float:
-		a = int(a)
-	if type(b) == float:
-		b = int(b)
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
 
-	return a + b
+    return a + b
