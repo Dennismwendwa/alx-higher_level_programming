f683272c519492e29f413ec502f86a70fa503464
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-10 18:20:32 +0300

    list sorted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,11 +6,8 @@ This is class Mylist with its attributes and methods
 
 class MyList(list):
     """ this are the attributes"""    
-    def __init__(self):
-        """contractor of the class """
-        super().__init__()
 
-    """this method prints sorted list"""
     def print_sorted(self):
+        """this method prints sorted list"""
         lists = sorted(self)
         print(lists)
