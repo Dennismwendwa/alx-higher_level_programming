77849aff71020b13d1f2fbf287984cbaf752492f
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-10 10:20:43 +0300

    checking type

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""checking if is instance of class"""
+
+
+"""this method checkis if is instance"""
+def is_same_class(obj, a_class):
+    
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
