7b0c0a5ccaf3590644c2008c18b02731fb0cffa8
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-10 17:44:22 +0300

    square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,37 +1,6 @@
 #!/usr/bin/python3
 """this is class geomentry"""
-
-
-class BaseGeometry:
-    """this are the attributes"""
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    """this method validates value"""
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """This is class base rectangle"""
-    def __init__(self, width, height):
-        """this are the attributes of the class"""
-        self.__width = width
-        self.integer_validator("width", width)
-        self.__height = height
-        self.integer_validator("height", height)
-
-    """this method returns area"""
-    def area(self):
-        return self.__width * self.__height
-
-    """this method return str reprecentation of the rectangle class"""
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
@@ -41,8 +10,12 @@ class Square(Rectangle):
         """this are the attributes"""
         self.__size = size
         self.integer_validator("size", size)
-        super().__init__(self.__size, self.__size)
+       #  super().__init__(self.__size, self.__size)
 
     """This method returns the area"""
     def area(self):
-        return super().area()
+        return self.__size * self.__size
+
+    """this is str method"""
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
