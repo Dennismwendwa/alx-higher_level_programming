1fe0884f740d3e7da620f129db99d5135f335a7a
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-10 09:55:01 +0300

    sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This is class Mylist with its attributes and methods
+"""
+
+
+class MyList(list):
+    """ this are the attributes"""
+    
+    """this method prints sorted list"""
+    def print_sorted(self):
+        lists = list(self)
+        lists.sort()
+        print(lists)
