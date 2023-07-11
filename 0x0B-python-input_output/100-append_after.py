12bea1bbc06f8694bac4743dbabc76f4c2ada006
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-11 16:45:44 +0300

    append

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,11 +6,15 @@ def append_after(filename="", search_string="", new_string=""):
     """This function searches and adds string
     Arguments:
     """
-    with open(filename, mode="r+", encoding="utf-8") as file:
-        lines = file.readlines()
-        file.seek(0)
-        for line in lines:
-            file.write(line)
-            if search_string in line:
-                file.write(new_string + "\n")
-        file.truncate()
+    add = []
+    with open(filename, mode="r", encoding="utf-8") as file:
+
+
+        for lin in file:
+            add += [lin]
+            if lin.find(search_string) != -1:
+                add += [new_string]
+
+
+        with open(filename, mode="w", encoding="utf-8") as file:
+            file.write("".join(add))
