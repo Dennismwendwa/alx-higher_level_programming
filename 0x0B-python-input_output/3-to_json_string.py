0b5590291a1e26c2e03b8fb9768f510a895bfab2
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-11 15:18:37 +0300

    from json to string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-import json
 """This function return json string"""
 
 
+import json
+
 def to_json_string(my_obj):
+    """This function returns json strings
+    Arguments:
+        my_obj: Object
+    """
     return json.dumps(my_obj)
