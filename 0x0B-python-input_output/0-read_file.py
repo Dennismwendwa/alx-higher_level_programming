da29e07220fdc8f3f8aadac5f9b1b89b422d7d22
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-11 15:24:06 +0300

    write to file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     This function writes into file
     """
     with open(filename, mode="r", encoding="utf-8") as file:
-        print(file.read())
+        print(file.read(), end="")
