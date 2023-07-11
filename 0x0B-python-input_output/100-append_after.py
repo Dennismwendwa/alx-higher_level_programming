87cd7d857035efb9a42a553c1211bca0ec9beecd
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-11 16:48:52 +0300

    append to end

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -9,12 +9,10 @@ def append_after(filename="", search_string="", new_string=""):
     add = []
     with open(filename, mode="r", encoding="utf-8") as file:
 
-
         for lin in file:
             add += [lin]
             if lin.find(search_string) != -1:
                 add += [new_string]
-
 
         with open(filename, mode="w", encoding="utf-8") as file:
             file.write("".join(add))
