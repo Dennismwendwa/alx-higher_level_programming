c24823f9a43ee01186fc1a34c4ef97d662215a09
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-15 09:05:08 +0300

    setting attributes by args or kwangs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,18 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    """This method sets attributes using argsa and kwargs"""
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
