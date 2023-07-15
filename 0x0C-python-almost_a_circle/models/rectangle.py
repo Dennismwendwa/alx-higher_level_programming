b0dec2a83bfd356924e14ce16f791a2a9af729e3
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-15 07:52:13 +0300

    using args to assign attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,3 +98,16 @@ class Rectangle(Base):
                 f"[Rectangle] ({self.id}) {self.x}/{self.y} - "
                 f"{self.width}/{self.height}"
                 )
+
+    """this method uses *args to assign attributes"""
+    def update(self, *args):
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
