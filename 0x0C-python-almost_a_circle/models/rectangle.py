3ed60e0542ebfa2fdc71e461527772cc2c5a027e
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-15 06:37:16 +0300

    added type and value checks for all atributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -31,6 +31,10 @@ class Rectangle(Base):
     """This method sets width"""
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     """this method returns height"""
@@ -41,6 +45,10 @@ class Rectangle(Base):
     """this method sets the height"""
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     """This method returns x"""
@@ -51,6 +59,10 @@ class Rectangle(Base):
     """this method sets x"""
     @x.setter
     def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     """This method returns y"""
@@ -61,4 +73,8 @@ class Rectangle(Base):
     """this method sets y"""
     @y.setter
     def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
