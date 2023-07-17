01e7e8d2721ffdd8c1b528ee6b9d4b0bdb1f6803
Dennismwendwa <dennismusembi2@gmail.com>
2023-07-17 04:20:39 +0300

    added tests for base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,9 @@
 """this are the test for base class"""
 
 
+from turtle import Turtle
+from unittest.mock import patch, MagicMock, call
+import csv
 import os
 from models.base import Base
 import unittest
@@ -10,7 +13,7 @@ from models.square import Square
 
 
 class TestBase(unittest.TestCase):
-
+    """this is test for base class"""
     def test_id_assignment(self):
         base1 = Base()
         base2 = Base()
@@ -24,15 +27,16 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base4.id, 500)
         self.assertEqual(base5.id, 3)
 
+    """this is test for to_json_string method"""
     def test_to_json_string(self):
-        
+
         list_d = [{"id": 1}, {"id": 100}, {"id": 2}]
         json_string = Base.to_json_string(list_d)
 
         outcome = '[{"id": 1}, {"id": 100}, {"id": 2}]'
         self.assertEqual(json_string, outcome)
 
-
+    """this is test for save_to_file"""
     def test_save_to_file(self):
 
         rect1 = Rectangle(4, 6)
@@ -46,8 +50,82 @@ class TestBase(unittest.TestCase):
         assert os.path.exists("Rectangle.json")
         assert os.path.exists("Square.json")
 
+    """this is test for create method"""
     def test_create(self):
         sset = {"id": 2}
         objects = Base(**sset)
 
         self.assertEqual(objects.id, 2)
+
+    """this test for load_from_file"""
+    def test_load_from_file(self):
+        filename = "Rectangle.json"
+
+        with open(filename, mode="w", encoding="utf-8") as file:
+            file.write('[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8},'
+                       ' {"id": 2, "width": 2, "height": 4, "x": 0, "y": 0}]')
+
+        instances = Rectangle.load_from_file()
+
+        self.assertEqual(len(instances), 2)
+
+        self.assertEqual(instances[0].id, 1)
+        self.assertEqual(instances[0].width, 10)
+        self.assertEqual(instances[0].height, 7)
+        self.assertEqual(instances[0].x, 2)
+        self.assertEqual(instances[0].y, 8)
+
+        self.assertEqual(instances[1].id, 2)
+        self.assertEqual(instances[1].width, 2)
+        self.assertEqual(instances[1].height, 4)
+        self.assertEqual(instances[1].x, 0)
+        self.assertEqual(instances[1].y, 0)
+
+        os.remove(filename)
+
+    """this is test for load from csv"""
+    def test_load_from_file_csv(self):
+        filename = "Square.csv"
+
+        with open(filename, mode="w", encoding="utf-8") as file:
+            file.write("1,7,0,0\n2,9,3,3")
+
+        instances = Square.load_from_file_csv()
+
+        self.assertEqual(len(instances), 2)
+
+        self.assertEqual(instances[0].id, 1)
+        self.assertEqual(instances[0].size, 7)
+        self.assertEqual(instances[0].x, 0)
+        self.assertEqual(instances[0].y, 0)
+
+        self.assertEqual(instances[1].id, 2)
+        self.assertEqual(instances[1].size, 9)
+        self.assertEqual(instances[1].x, 3)
+        self.assertEqual(instances[1].y, 3)
+
+        os.remove(filename)
+
+    """This is test for save to csv"""
+    def test_save_to_file_csv(self):
+        r1 = Rectangle(11, 12, 13, 14)
+        r2 = Rectangle(20, 21, 22, 23)
+        s1 = Square(33, 34, 35)
+
+        Rectangle.save_to_file_csv([r1, r2])
+        Square.save_to_file_csv([s1])
+
+        with open("Rectangle.csv", mode="r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+
+            self.assertEqual(len(rows), 2)
+            self.assertEqual(rows[0], ["10", "11", "12", "13", "14"])
+            self.assertEqual(rows[1], ["11", "20", "21", "22", "23"])
+
+        with open("Square.csv", mode="r", encoding="utf-8") as file:
+            reader = csv.reader(file)
+            rows = list(reader)
+
+            self.assertEqual(len(rows), 1)
+            self.assertEqual(rows[0], ["12", "33", "34", "35"])
