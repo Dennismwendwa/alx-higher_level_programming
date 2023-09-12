3bc269a1bd3bf8d8ca5d4b80f054024341a495e6
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-12 15:21:55 +0300

    corrected semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,14 +6,14 @@ class Square extends lastSquare {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
-	}
-	for (let k = 0; k < this.height; k++) {
+    }
+    for (let k = 0; k < this.height; k++) {
       let row = '';
       for (let w = 0; w < this.width; w++) {
         row += c;
-	  }
+      }
       console.log(row);
-	}
+    }
   }
 }
 
