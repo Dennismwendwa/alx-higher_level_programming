4e2a3be171def34c7e8c8410858ca562944c12d9
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-12 09:44:05 +0300

    methods

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+      return {};
+	}
+	this.width = w;
+    this.height = h;
+  }
+
+  print() {
+    for (let k = 0; k < this.height; k++) {
+      let row = '';
+	  for (let p = 0; p < this.width; p++) {
+        row += 'X';
+	  }
+      console.log(row);
+	}
+  }
+
+  rotate() {
+  const swap = this.width;
+  this.width = this.height;
+  this.height = swap;
+  }
+
+  double() {
+    this.width *= 2;
+	this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
