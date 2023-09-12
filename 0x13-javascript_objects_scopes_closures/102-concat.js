9f136ec02f8f4d9512b709049084ed4672802151
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-12 15:54:28 +0300

    concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const file1 = process.argv[2];
+const file2 = process.argv[3];
+const file1Content = fs.readFileSync(file1);
+const file2Content = fs.readFileSync(file2);
+
+const allContent = file1Content + file2Content;
+
+fs.writeFile(process.argv[4], allContent, 'utf-8', (errors) => {
+  if (errors) {
+    console.error(errors);
+    return;
+  }
+});
