13cfcaed3e018afe0708bd8ddbafc07dc0fbff2b
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-15 07:25:01 +0300

    pycodesytle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,41 @@
 #!/usr/bin/python3
+"""
+Script to select all states
+"""
 
-import MySQLdb
-import sys
+if __name__ == "__main__":
 
-if len(sys.argv) != 4:
-    print(
-            "Usage: {} <mysql_username> <mysql_password> <database_name>"
-            .format(sys.argv[0])
-            )
-    sys.exit(1)
+    import MySQLdb
+    import sys
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    try:
-        db = MySQLdb.connect(
-                host="localhost", port=3306, user=username, password=password,
-                db=database_name
+    if len(sys.argv) != 4:
+        print(
+                "Usage: {} <mysql_username> <mysql_password> <database_name>"
+                .format(sys.argv[0])
                 )
-        cursor = db.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        states = cursor.fetchall()
+        sys.exit(1)
 
-        for k in states:
-            print(k)
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
 
-    except MySQLdb.Error as e:
-        pass
-    sys.exit(1)
+        try:
+            db = MySQLdb.connect(
+                    host="localhost", port=3306,
+                    user=username, password=password,
+                    db=database_name
+                    )
+            cursor = db.cursor()
+            cursor.execute("SELECT * FROM states ORDER BY id ASC")
+            states = cursor.fetchall()
 
-    finally:
-        cursor.close()
-        db.close()
+            for k in states:
+                print(k)
+
+        except MySQLdb.Error as e:
+            pass
+        sys.exit(1)
+
+        finally:
+            cursor.close()
+            db.close()
