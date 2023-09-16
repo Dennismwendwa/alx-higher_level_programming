5be9efa81818904025f30c87ba40af7c66153563
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-16 05:39:17 +0300

    created class states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,6 +13,6 @@ class State(Base):
 
     id = Column(
             Integer, primary_key=True,
-            nullable=False, autoiincrement=True
+            nullable=False, autoincrement=True
             )
     name = Column(String(128), nullable=False)
