3374b0ee0242fee82c189948d88deea517d8445c
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-28 12:17:28 +0300

    body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # script that returns size of the body of url
 
 url="$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # script that uses DELETE request
 
 url="$1"
