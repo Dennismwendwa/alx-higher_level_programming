0b44bb79ef1afed7a507b197ea5ad11da43ea1b5
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-28 10:52:36 +0300

    finding peak in list of integers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Function that finds a peak in list of integers"""
+
+
+"""This function finds peak"""
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+    return list_of_integers[low]
