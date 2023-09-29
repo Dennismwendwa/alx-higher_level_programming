43e41ebcac799caa619e87dfb471162b3b327a38
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-29 11:33:18 +0300

    auth with github api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""this script sends request to github"""
+import requests
+from sys import argv
+
+
+username = argv[1]
+token = argv[2]
+
+url = f"https://api.github.com/user"
+
+auth = (username, token)
+
+try:
+    response = requests.get(url, auth=auth)
+    response.raise_for_status()
+
+    user_data = response.json()
+    user_id = user_data.get("id")
+
+    if user_id is not None:
+        print(user_id)
+    else:
+        print("None")
+
+except requests.exceptions.RequestException as e:
+    print("None")
