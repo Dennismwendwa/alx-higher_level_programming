e2dd6f5bb50c83f4fff3a3dd58819dcd1c0290dd
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-30 01:38:39 +0300

    json request

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,25 +4,26 @@ import requests
 from sys import argv
 
 
-if len(argv) == 2:
-    q = argv[1]
-else:
-    q = ""
+if __name__ == "__main__":
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
 
-url = "http://0.0.0.0:5000/search_user"
-data = {"q": q}
-
-try:
-    response = requests.post(url, data=data)
-    response.raise_for_ststus()
+    url = "http://0.0.0.0:5000/search_user"
+    data = {"q": q}
 
     try:
-        json_data = response.json()
-        if json_data:
-            print("[{}] {}".format(json_data["id"], json_data["name"]))
-        else:
-            print("No result")
-    except ValueError:
-        pass
-except requests.exceptions.RequestException as e:
-    print("{}".format(e))
+        response = requests.post(url, data=data)
+        response.raise_for_ststus()
+
+        try:
+            json_data = response.json()
+            if json_data:
+                print("[{}] {}".format(json_data["id"], json_data["name"]))
+            else:
+                print("No result")
+        except ValueError:
+            print"Not a valid JSON")
+    except requests.exceptions.RequestException as e:
+        print("{}".format(e))
