d27be167d7654406ed4cb2cfad207faa2367be54
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-30 01:56:17 +0300

    sending post request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,15 +4,16 @@ import requests
 from sys import argv
 
 
-url = argv[1]
-email = argv[2]
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
 
-data = {"email": email}
+    data = {"email": email}
 
-try:
-    response = requests.post(url, data=data)
-    response.raise_for_status()
-    print("Your email is:", response.text)
+    try:
+        response = requests.post(url, data=data)
+        response.raise_for_status()
+        print("Your email is:", response.text)
 
-except requests.exceptions.RequestException as e:
-    pass
+    except requests.exceptions.RequestException as e:
+        pass
