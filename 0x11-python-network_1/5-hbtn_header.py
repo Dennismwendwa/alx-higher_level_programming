3cf068a000f31aa4dd577c48ff9fb8d8b0d36400
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-29 09:28:47 +0300

    X-Requst-Id

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""this script uses requests to print X-Request-Id"""
+import requests
+from sys import argv
+
+
+url = argv[1]
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+
+    x_request_id = response.headers.get("X-Request-Id")
+
+    if x_request_id:
+        print(x_request_id)
+
+except requests.exceptions.RequestException as e:
+    pass
