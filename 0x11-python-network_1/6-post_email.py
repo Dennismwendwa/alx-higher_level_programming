cc70f7fd6a4a7e50a1187196e6c2b94fd28c67e7
Dennismwendwa <dennismusembi2@gmail.com>
2023-09-30 02:00:31 +0300

    sending post request

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     try:
         response = requests.post(url, data=data)
         response.raise_for_status()
-        print("Your email is:", response.text)
+        print(response.text)
 
     except requests.exceptions.RequestException as e:
         pass
