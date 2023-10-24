3e845281a94a45e9e5ed15d779766b2f9511f3d0
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-10-24 15:18:38 +0300

    writting to file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// reading strings to file
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('File written successfully.');
+  }
+});
