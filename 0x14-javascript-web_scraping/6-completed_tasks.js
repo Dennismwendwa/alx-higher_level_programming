966a6e79c04c1865f6e948dda9a2dc9d2c39e9de
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-10-24 16:48:24 +0300

    counting completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+// script that computes number of tasks completed by user id
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    if (response.statusCode === 200) {
+      const todos = JSON.parse(body);
+
+      const completedTasks = {};
+
+      todos.forEach((todo) => {
+        if (completedTasks[todo.userId]) {
+          completedTasks[todo.userId]++;
+        } else {
+          completedTasks[todo.userId] = 1;
+        }
+      });
+      console.log(completedTasks);
+    }
+  }
+});
