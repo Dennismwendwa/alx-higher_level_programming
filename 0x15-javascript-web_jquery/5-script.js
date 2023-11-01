17a8997ed97297def09c88f89a87b9112870ae11
Dennis Musembi Mwendwa <dennismusembi2@gmail.com>
2023-11-01 16:30:14 +0300

    updating the DOM elements

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,9 @@
+// adding elements
+
+$(document).ready(function() {
+  $('#add_item').click(function() {
+    var newItem = $('<li>Item</li>');
+
+    $('.my_list').append(newItem);
+  });
+});
---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+// changing element text
+
+$(document).ready(function() {
+  $('#update_header').click(function() {
+    $('header').text('New Header!!!');
+  });
+});
